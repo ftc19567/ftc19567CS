b4a9902de4f99765e318f460de5072f7da5e685a
Clement12341 <111813679+Clement12341@users.noreply.github.com>
2023-11-10 21:12:02 -0600

    Fixed values for blue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/vision/OpenCV/TeamPropDetectorPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/vision/OpenCV/TeamPropDetectorPipeline.java
@@ -13,14 +13,29 @@ public class TeamPropDetectorPipeline extends OpenCvPipeline {
     Telemetry telemetry; // Info
     Mat mat = new Mat(); // Matrix of the Camera (essentially the camera image)
 
+
     // Creating ROIs for the camera (Regions of Interest) to see where the object is located.
     static final Rect LEFT_ROI = new Rect (
             new Point(1, 1),
-            new Point(300, 300));
+            new Point(200, 359));
+
+    static final Rect MIDDLE_ROI = new Rect (
+            new Point(201, 1),
+            new Point(438, 359));
 
     static final Rect RIGHT_ROI = new Rect (
-            new Point(301, 1),
-            new Point(600, 300));
+            new Point(439, 1),
+            new Point(639, 359));
+
+
+    //lower hsv values to detect the color. Values can be modified and tuned as necessary.
+    Scalar lowHSVBlue = new Scalar (46.8, 96.3, 41.1);
+    // same here. Values can be tuned using eocv sim in valueFinder
+    Scalar highHSVBlue = new Scalar (137.4, 255, 255);
+
+    //Defining colors for the ROI, thhis is just design.
+
+    Scalar rectColor = new Scalar(255.0, 0.0, 0.0);
 
 
     public TeamPropDetectorPipeline (Telemetry t) { telemetry = t;} //Passing telemetry as a constructor
@@ -29,36 +44,65 @@ public class TeamPropDetectorPipeline extends OpenCvPipeline {
     public Mat processFrame(Mat input) {
         //Converting the Matrix from rgb to HSV (Hue, Saturation, and Value/Brightness)
         Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
-        //lower hsv values to detect the color. Values can be modified and tuned as necessary.
-        Scalar lowHSVBlue = new Scalar (221, 68.05, 42);
-        // same here. Values haven't been tuned yet.
-        Scalar highHSVBlue = new Scalar (235, 100, 100);
 
         //Thresholding, turning pixels meeting the hsv range into white and the rest into black.
         Core.inRange(mat, lowHSVBlue, highHSVBlue, mat);
 
         //Extracting the ROIs from the image
         Mat left = mat.submat(LEFT_ROI);
+        Mat middle = mat.submat(MIDDLE_ROI);
         Mat right = mat.submat(RIGHT_ROI);
 
         //Finding the percent of the white pixels. Determine it by adding the white pixels together (.val [0} finds them) and
         // dividing by the total area and the max greyscale, 255.
         double leftValue = Core.sumElems(left).val[0] / LEFT_ROI.area() / 255;
+        double middleValue = Core.sumElems(middle).val[0] / MIDDLE_ROI.area() / 255;
         double rightValue = Core.sumElems(right).val[0] / RIGHT_ROI.area() / 255;
 
         //Prevents memory leaking by release the Mat right after using them.
         left.release();
+        middle.release();
         right.release();
 
-        //telemtry for info
-        telemetry.addData("Left raw value: ", (int) Core.sumElems(left).val[0]);
-        telemetry.addData("Right raw value: ", (int) Core.sumElems(right).val[0]);
-        telemetry.addData("Left percentage: ", Math.round(leftValue *100) + "%");
-        telemetry.addData("Right percentage: ", Math.round(rightValue *100) + "%");
 
+
+        //So you can see the ROIs
+        Imgproc.rectangle(mat, LEFT_ROI, rectColor, 2);
+        Imgproc.rectangle(mat, MIDDLE_ROI, rectColor, 2);
+        Imgproc.rectangle(mat, RIGHT_ROI, rectColor, 2);
+
+        //Rounding the percents
+
+        int leftPercent = (int) Math.round(leftValue *100);
+        int middlePercent = (int) Math.round(middleValue *100);
+        int rightPercent = (int) Math.round(rightValue *100);
+
+
+        //telemtry for info
+        //telemetry.addData("Left raw value: ", (int) Core.sumElems(left).val[0]);
+        //telemetry.addData("Right raw value: ", (int) Core.sumElems(right).val[0]);
+        telemetry.addData("Left percentage: ", leftPercent + "%");
+        telemetry.addData("Middle percentage: ", middlePercent + "%");
+        telemetry.addData("Right percentage: ", rightPercent + "%");
+
+        if (leftPercent == middlePercent && leftPercent == rightPercent) {
+            telemetry.addData("Side: ", "None Detected");
+        } else if (leftPercent > middlePercent && leftPercent > rightPercent) {
+            telemetry.addData("Side: ", "Left");
+        } else if (rightPercent > leftPercent && rightPercent > middlePercent) {
+            telemetry.addData("Side: ", "Right");
+        } else {
+            telemetry.addData("Side: ", "Middle");
+
+
+        }
+
+        //so telemetry won't clutter
         telemetry.update();
 
 
+
+        //return the mat to the ui
         return mat;
 
     }
