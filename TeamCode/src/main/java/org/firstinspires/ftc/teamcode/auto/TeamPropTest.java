ffd8b9fe133d626043d5f16d4aa8a2735d93fef8
AndrxwWxng <andrewwang123118@gmail.com>
2023-11-11 12:38:09 -0600

    Stuff + opencv + RR

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/TeamPropTest.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.auto.pipeline.TeamPropHSVPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Autonomous(name="AutoTestTele")
+public class TeamPropTest extends LinearOpMode {
+
+    OpenCvCamera camera;
+    WebcamName webcam1;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        int cameraMonitorViewId = hardwareMap.appContext
+                .getResources().getIdentifier("cameraMonitorViewId",
+                        "id", hardwareMap.appContext.getPackageName());
+        webcam1 = hardwareMap.get(WebcamName.class, "Webcam 1");
+        camera = OpenCvCameraFactory.getInstance()
+                .createWebcam(webcam1, cameraMonitorViewId);
+
+        TeamPropHSVPipeline pipeline = new TeamPropHSVPipeline(telemetry);
+
+        camera.setPipeline(pipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(640, 360, OpenCvCameraRotation.UPRIGHT);
+            }
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
+        waitForStart();
+
+    }
+
+}
