d9d244788c037abd099890c99379b29cbbd2b229
AndrxwWxng <andrewwang123118@gmail.com>
2023-12-23 12:36:26 -0600

    clemhent qanoidjdi9kisz=daevbc xasada n dsf

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -13,31 +13,37 @@ public class MeepMeepTesting {
         MeepMeep meepMeep = new MeepMeep(700);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                //Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(63.1044330668311, 40, Math.toRadians(180), Math.toRadians(180), 14.26)
                 .followTrajectorySequence(drive ->
-                                drive.trajectorySequenceBuilder(new Pose2d(-34.5, -61.5, Math.toRadians(-90)))
-                                        .lineTo(new Vector2d(-34.5, -5))
-                                        .lineToLinearHeading(new Pose2d(59.6, -11.5, Math.toRadians(0)))
-                                        //.splineToLinearHeading(new Pose2d(60, 60, Math.toRadians(180)), Math.toRadians(0))
+                                drive.trajectorySequenceBuilder(new Pose2d(14.5, -61.5, Math.toRadians(-90)))
+                                        .lineToLinearHeading(new Pose2d (20, -40, Math.toRadians(-90)))
+                                        .back(-5)
+                                        .splineToLinearHeading(new Pose2d(43, -42, Math.toRadians(180)), Math.toRadians(0))
+                                        .waitSeconds(0.25)
+                                        .back(10)
 
 
-
-
-        //.lineToLinearHeading(new Pose2d(47, -42, Math.toRadians(180)))
-                                //.splineToLinearHeading(new Pose2d(51, -61, Math.toRadians(0)), Math.toRadians(0))
-                                //.splineToLinearHeading(new Pose2d(47, -42, Math.toRadians(-90)), Math.toRadiamns(-90))
-
-                                //.splineToLinearHeading(new Pose2d(47, -42, Math.toRadians(-90)), Math.toRadians(-90))
-                                //.turn(Math.toRadians(-90))
-                                //.lineToLinearHeading(new Pose2d(47, -58, Math.toRadians(-180)))
-                                //.forward(-15)
-                                .build());
-
-        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
-                .setDarkMode(true)
-                .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
-                .start();
+                                        .splineToLinearHeading(new Pose2d(30, -60, Math.toRadians(180)), Math.toRadians(180))
+                                        .waitSeconds(0.25)
+                                        .lineToLinearHeading(new Pose2d(-13, -60, Math.toRadians(180)))
+                                        .lineToLinearHeading(new Pose2d(-12.5, -10, Math.toRadians(180)))
+                                        .waitSeconds(0.25)
+                                        .lineToLinearHeading(new Pose2d(-58, -11, Math.toRadians(180)))
+                                        
+                                        .forward(2.5)
+                                        
+                                        .lineToLinearHeading(new Pose2d(45, -10, Math.toRadians(180)))
+                                        .waitSeconds(1)
+                                        .splineToLinearHeading(new Pose2d(40, -28, Math.toRadians(180)), Math.toRadians(0))
+                                        .waitSeconds(0.25)
+                                        .back(10)
+                                       
+                                        .forward(7)
+                                      
+                                        .strafeLeft(30)
+                                        .back(10)
+                                        .build()
+                );
     }
 }
