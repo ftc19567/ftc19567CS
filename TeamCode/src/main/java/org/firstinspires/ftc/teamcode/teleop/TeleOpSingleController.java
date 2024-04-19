28b6d09a33e992629b61df864ddbdd82787d6e5e
Clement12341 <111813679+Clement12341@users.noreply.github.com>
2024-04-18 22:44:26 -0500

    for single controller (no place)
    also no quick hang

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpSingleController.java
@@ -0,0 +1,382 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+
+
+import static java.lang.Thread.sleep;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.mechanisms.Arm;
+import org.firstinspires.ftc.teamcode.mechanisms.Intake;
+//import org.firstinspires.ftc.teamcode.mechanisms.ThreadAD;
+//import org.firstinspires.ftc.teamcode.mechanisms.ThreadAU;
+
+
+@TeleOp
+public class TeleOpSingleController extends OpMode {
+
+
+    private DcMotor frontLeftMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backRightMotor;
+    public Servo turnServo;
+    public Servo planeServo;
+    public Servo intakeServo;
+
+    public Arm arm;
+    private DcMotor lowMotor, highMotor;
+    private Intake intake;
+    private boolean liftArmUp = false;
+    private boolean liftArmDown = false;
+    private boolean boxUp = false;
+    private boolean boxDown = false;
+    private boolean reset = false;
+    private boolean plane = false;
+    private boolean hanging = false;
+    private boolean hang = false;
+    private boolean release = false;
+
+    private boolean active1 = false;
+
+    private boolean active2 = false;
+
+    private boolean autoTurnServo = true;
+
+    private double servoPosCalc = 1;
+
+    private boolean timerUpB = false;
+
+    private boolean timerUpFB = false;
+
+    private boolean timerDownB = false;
+
+    private double powerMult = 1;
+
+
+
+
+
+    ElapsedTime timerUp = new ElapsedTime();
+    ElapsedTime timerUpFinsh = new ElapsedTime();
+    ElapsedTime timerDown = new ElapsedTime();
+
+
+    //ThreadAU threadAU = new ThreadAU();
+    //ThreadAD threadAD = new ThreadAD();
+
+
+
+
+    @Override
+    public void init() {
+
+        //MECANUM
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+
+        lowMotor = hardwareMap.get(DcMotor.class, "lowMotor");
+        highMotor = hardwareMap.get(DcMotor.class, "highMotor");
+
+
+
+        //ARM
+        arm = new Arm(hardwareMap, telemetry);
+
+        /*lowMotor = hardwareMap.get(DcMotor.class, "lowMotor");
+        highMotor = hardwareMap.get(DcMotor.class, "highMotor");
+
+        lowMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        highMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        lowMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        highMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        lowMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        highMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+         */
+
+        //TURNING SERVO
+        turnServo = hardwareMap.get(Servo.class, "turnServo");
+
+        turnServo.setPosition(0.67);
+        Arm.setPosition(1, 0);
+
+        telemetry.addData("Encoder :", arm.lowGetPosition());
+
+        //AIRPLANE
+        planeServo = hardwareMap.get(Servo.class, "planeServo");
+        planeServo.setPosition(1);
+
+        //ACTIVE INTAKE
+        intakeServo = hardwareMap.get(Servo.class, "intakeServo");
+        intakeServo.setPosition(0.796);
+
+
+
+        telemetry.update();
+
+    }
+    // Declare our motors
+    // Make sure your ID's match your configuration
+
+
+
+    @Override
+    public void loop() {
+
+        /*
+        if (gamepad1.left_bumper) {
+            powerMult = 0.5;
+            telemetry.addData("Current Speed : ", (powerMult * 100) + "%");
+        }
+
+        if (gamepad1.right_bumper) {
+            powerMult = 1;
+            telemetry.addData("Current Speed : ", (powerMult * 100) + "%");
+        }
+
+         */
+
+
+
+        //Mecanum
+        double y = -gamepad1.left_stick_y * powerMult ; // Remember, Y stick value is reversed
+        double x  = gamepad1.left_stick_x * 1.1 * powerMult; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x * 0.5 * powerMult;
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
+
+        /*telemetry.addData("highMotor", arm.highGetPosition());
+        telemetry.addData("lowMotor", arm.lowGetPosition());
+         */
+        telemetry.update();
+
+
+        if (gamepad1.a && !liftArmUp && timerUpB == false && timerUpFB == false) {
+            if (arm.highGetPosition() <= 1000) {
+                turnServo.setPosition(1);
+                liftArmUp = true;
+                timerUp.reset();
+                timerUpB = true;
+
+            } }else if (!gamepad1.a) {liftArmUp = false;}
+
+        if (timerUp.milliseconds() >= 100 && timerUpB == true) {
+            arm.setPosition(0.6, 1750);
+            timerUpB = false;
+            timerUpFinsh.reset();
+            timerUpFB = true;
+        }
+        if (timerUpFinsh.milliseconds() >= 700 && timerUpFB == true) {
+            turnServo.setPosition(0.24);
+            timerUpFB = false;
+        }
+
+
+        //LiftArmDown
+
+        if (gamepad1.b && !liftArmDown && timerDownB == false) {
+            if (arm.highGetPosition() > 1000) {
+                turnServo.setPosition(1);
+                arm.setPosition(1, 5);
+                liftArmDown = true;
+                timerDownB = true;
+                timerDown.reset();
+            }} else if (!gamepad1.b) {liftArmDown = false;}
+
+        if (timerDown.milliseconds() >= 1000  && timerDownB == true) {
+            turnServo.setPosition(0.67);
+            timerDownB = false;
+        }
+
+
+
+        if (gamepad1.left_bumper) {
+            Arm.negativeArmPower();
+
+        }
+
+
+        if (gamepad1.left_trigger > 0.5) {
+            Arm.positiveArmPower();
+
+        }
+
+        //Turning Servo
+        if (gamepad1.x && !boxDown) {
+            turnServo.setPosition(turnServo.getPosition() + 0.03);
+            boxDown = true;
+        } else if (!gamepad1.x) {
+            boxDown = false;
+        }
+        if (gamepad1.y && !boxUp) {
+            turnServo.setPosition(turnServo.getPosition() - 0.03);
+            boxUp = true;
+        } else if (!gamepad1.y) {
+            boxUp = false;
+        }
+
+
+
+/*
+        //turning servo
+        if (arm.lowGetPosition() > 1000) {
+            servoPosCalc = 0.7 - (arm.lowGetPosition()/1600 * 0.35);
+        }
+
+        if (gamepad1.y) {autoTurnServo = true;}
+
+        if (gamepad1.b) {autoTurnServo = false;}
+
+        if (!autoTurnServo) {
+            if (gamepad1.dpad_up) {
+                turnServo.setPosition(turnServo.getPosition() - 0.1);
+            }
+
+            if (gamepad1.dpad_down) {
+                turnServo.setPosition(turnServo.getPosition() + 0.1);
+            }
+        }  else {
+            turnServo.setPosition(servoPosCalc);
+        }
+        */
+
+
+
+
+        //intake
+        intake = new Intake(hardwareMap, telemetry);
+
+        if (gamepad1.right_trigger > 0) {
+            intake.rollingIntake(1);
+        }
+
+
+
+        //ejects pixels
+        intake.ejection((gamepad1.right_bumper));
+
+
+        /*
+        //airplane
+        if (gamepad1.dpad_up && !plane) {
+            planeServo.setPosition(0.5);
+            plane = true;
+        } else if (!gamepad1.dpad_up){
+            plane = false;
+        }
+        if (gamepad1.left_trigger > 0) {
+            gamepad1.rumble(100);
+        }
+
+         */
+
+
+        //hanging
+        if (gamepad1.dpad_up && !hanging) {
+            int i = 1;
+            if(i == 1) {
+                turnServo.setPosition(1);
+                try {
+                    sleep(300);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                Arm.setPosition(1, 1100);
+                //TUNE ABOVE VALUE FOR WHEN ARM IS FULLY STRAIGHT
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                turnServo.setPosition(0.3);
+                //TUNE VALUE FOR STRAIGHT BOX
+            }
+            hanging = true;
+        } else if (!gamepad1.dpad_up) {
+            hanging = false;
+        }
+
+        //release turning servo
+        if (gamepad1.dpad_down && !release) {
+            turnServo.setPosition(0.15);
+            try {
+                sleep(150);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            turnServo.getController().pwmDisable();
+            release = true;
+        } else if (!gamepad1.dpad_down) {
+            release = false;
+        }
+
+        /*
+        //hang
+        if (gamepad1.b && !hang) {
+            Arm.setPosition(1, 100);
+            hang = true;
+        } else if (!gamepad1.b) {
+            hang = false;
+        }
+
+         */
+
+
+
+        if (gamepad1.dpad_right && !active1) {
+            intakeServo.setPosition(intakeServo.getPosition() + 0.002);
+            active1 = true;
+        } else if (!gamepad1.dpad_right) {
+            active1 = false;
+        }
+
+        if (gamepad1.dpad_left && !active2) {
+            intakeServo.setPosition(intakeServo.getPosition() - 0.002);
+            active2 = true;
+        } else if (!gamepad1.dpad_left) {
+            active2 = false;
+        }
+
+
+        telemetry.addData("ServoPos : ", turnServo.getPosition());
+        telemetry.addData("ArmPos : ", lowMotor.getCurrentPosition());
+        telemetry.addData("liftUpB : ", liftArmUp);
+        telemetry.addData("liftDownB : ", liftArmDown);
+        telemetry.addData("planeServo Pos : ", planeServo.getPosition());
+        telemetry.addData("Active Intake :", intakeServo.getPosition());
+
+    }
+}
